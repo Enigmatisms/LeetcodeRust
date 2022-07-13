d5bb17288fc8cc4cd6b7fb3f8bb57237bb4d05c3
Qianyue He <984041003@qq.com>
2022-07-13 16:38:15 +0800

    Utterly false train of thoughts

--- fast_test/src/main.rs
+++ fast_test/src/main.rs
@@ -1,6 +1,19 @@
+type Link = Option<Box<ListNode>>;
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Link
+}
+
 fn main() {
-    let a = String::from("he is a fool");
-    let slice = &a[0..2];
-    let he: &str = "hes";
-    println!("{}", slice == he);
+    let mut a = ListNode{val:0, next:None};
+    let mut b = Some(Box::new(ListNode{val:1, next:Some(Box::new(a))}));
+    let mut c = b.clone();
+    c.as_mut().map(|inner| {
+        inner.next.as_mut().map(|a_val| {
+            a_val.val = 2;
+        });
+    });
+    println!("{}", b.as_ref().unwrap().next.as_ref().unwrap().val);
+    println!("{}", c.as_ref().unwrap().next.as_ref().unwrap().val);
 }
