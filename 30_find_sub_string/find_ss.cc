d248921df49772496170d388b42feecfaf9c4fdb
Enigmatisms <984041003@qq.com>
2023-09-18 00:58:41 +0800

    Re-sorted the folders

--- 30_find_sub_string/find_ss.cc
+++ 30_find_sub_string/find_ss.cc
@@ -9,6 +9,7 @@ public:
         // for each starting index, try to synthesis the sentence (at the same time keeping count of the usage of words)
         // note that: word can have duplicates, so one key might map to multiple words (thus we need counter)
         // word can also be the substring of other words, so one value (word) can have multiple keys
+        // z-algorithm (TODO compare with KMP)
     }
 
     void find_all_index(const std::string& haystack, const std::string& needle, std::vector<short>& all_idxs) const {
