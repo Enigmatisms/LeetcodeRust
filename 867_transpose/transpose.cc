ed7642d4219a0ba312e6f96c8958b2b780f7148d
Enigmatisms <984041003@qq.com>
2024-03-12 21:54:59 +0800

    Benchmarking tiled matrix transpose

--- 867_transpose/transpose.cc
+++ 867_transpose/transpose.cc
@@ -1,11 +1,27 @@
-#include <iostream>
+#include <random>
 #include <vector>
-#include <algorithm>
-#include <numeric>
+#include <iostream>
 #include <unordered_map>
-#include <unordered_set>
+#include "../ListNode.hpp"
 
 using namespace std;
+
+std::vector<std::vector<int>> generateRandomMatrix(int row, int col) {
+    // 创建一个随机数生成器
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    // 定义整数分布范围
+    std::uniform_int_distribution<> dis(1, 100); // 可以根据需要调整范围
+
+    // 生成随机矩阵
+    std::vector<std::vector<int>> matrix(row, std::vector<int>(col));
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            matrix[i][j] = dis(gen); // 生成随机整数
+        }
+    }
+    return matrix;
+}
 
 /**
  * runtime: 99.16%
@@ -24,10 +40,51 @@ public:
         }
         return result;
     }
+
+    // 分块转置(只针对大小为 16 的倍数的矩阵，用于 benchmarking)
+    vector<vector<int>> transpose_patch(vector<vector<int>>& matrix) {
+        size_t row = matrix.size(), col = matrix[0].size();
+        size_t rpatch = row >> 4, cpatch = col >> 4;
+        std::vector<std::vector<int>> result(col, std::vector<int>(row, 0));
+        for (size_t r = 0; r < rpatch; r++) {
+            size_t rbase = r << 4;
+            for (size_t c = 0; c < cpatch; c++) {
+                size_t cbase = c << 4;
+                for (size_t i = 0; i < 16; i++) {
+                    const auto& row = matrix[rbase + i];
+                    for (size_t j = 0; j < 16; j++) {
+                        result[cbase + j][rbase + i] = row[cbase + j];
+                    }
+                }
+            }
+        }
+        return result;
+    }   
 };
 
 int main(int argc, char** argv) {
     Solution sol;
+    TicToc timer;
+    int size = 4000, test_size = 100;
+    if (argc >= 3) {
+        test_size = atoi(argv[2]);
+    }
+    if (argc >= 2) {
+        size = atoi(argv[1]);
+    }
+    double total_origin = 0, total_patch = 0;
+    for (int i = 0; i < test_size; i++) {
+        auto matrix1 = generateRandomMatrix(size, size);
+        auto matrix2 = matrix1;
+        timer.tic();
+        auto result1 = sol.transpose(matrix1);
+        total_origin += timer.toc(); 
 
+        timer.tic();
+        auto result2 = sol.transpose(matrix2);
+        total_patch += timer.toc(); 
+        printf("%04d/%04d\n", i + 1, test_size);
+    }
+    printf("Benchmark: %.5lf ms and %.5lf ms\n", total_origin / test_size, total_patch / test_size);
     return 0;
 }
--- ListNode.hpp
+++ ListNode.hpp
@@ -7,6 +7,22 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <chrono>
+
+class TicToc {
+private:
+    std::chrono::system_clock::time_point tp;
+public:
+    void tic() {
+        tp = std::chrono::system_clock::now();
+    }
+
+    double toc() const {
+        auto dur = std::chrono::system_clock::now() - tp;
+        auto count = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
+        return static_cast<double>(count) / 1e3;
+    }
+};
 
 struct ListNode {
     int val;
